67976bd9160b78cf8cd8ea693ab6194307215cba
Pascal <pascal_haller@hotmail.com>
2013-11-07 20:03:47 +0100

    level kreations-versuch mit aliens

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -23,73 +23,38 @@ Q.UI.Text.extend("Score",{
     }
 });
 
-/**
- * The Cannon class which is at the bottom of the page.
- */
 
-Q.Sprite.extend("Cannon", {
-    init: function(p){
-        this._super({
-            asset: 'cannon.png',    // image
-            w: 110,                 // width
-            h: 68,                  // height
-            type: Q.SPRITE_DEFAULT | Q.SPRITE_FRIENDLY, // reacts to sprites mask
-            y: 680,                 // position
-            x: 512,
-            stepDistance: 50       // moving speed
-        }, p);
-        this.add('GunControls, gunControls');
-        Q.input.on('fire', this, "fireGun");
-
-//        this.on("hit.sprite",function(collision) {
-//            if(collision.obj.isA("AlienShot")) {
-//                Q.stageScene("CannonHit", 1);
-//                this.destroy();
-//              }
-//            });
+Q.Sprite.extend("CannonShot",{
+    init: function(p) {
+        this._super(p,{
+           asset: 'shoot.png',    // image
+            w: 20,
+            h: 20,
+            type: Q.SPRITE_DEFAULT | Q.SPRITE_FRIENDLY
+        });
 
     },
 
-    fireGun: function(){
-        console.log("firing...");
-        Q.audio.play("fire2.mp3");
+    step: function(dt){
+        this.p.y =this.p.y-2;
+        if(this.p.y < 0) this.destroy();
     }
+
 });
 
-
-
-    Q.TileLayer.extend("Alien", {
+    Q.Sprite.extend("Alien",{
         init: function(p){
-            this._super({
-
-                tileW: 32,  // Default tile width
-                tileH: 32,  // Default tile height
-                blockTileW: 10,  // Default pre-render size
-                blockTileH: 10,
-                type: Q.SPRITE_DEFAULT // Default type (for collisions)
-
-
-                   // gravity factor
-            }, p);
-            //this.add('2d, stepControls');
-            Q.input.on('fire', this, "fireGun");
-
-            this.on("hit",this,"collision");
-
-        },
-        step: function(dt) {
-            this.p.vy += dt * 2.8;
-
-            this.p.x += this.p.vx * dt;
-            this.p.y += this.p.vy * dt;
-        },
-        fireGun: function(){
-            console.log("firing...");
-            Q.audio.play("fire2.mp3");
+            this._super(p,{
+               asset:'invaders.png',
+                w:20,
+                h:20,
+                type:  Q.SPRITE_DEFAULT,
+                y: 80,                 // position
+                x: 412,
+                stepDistance: 50
+            });
         }
     });
-
-
 
     Q.TileLayer.extend("GameTiles",{
         init: function(p) {
@@ -133,4 +98,96 @@ Q.Sprite.extend("Cannon", {
         }
 
     });
+
+/**
+ * The Cannon class which is at the bottom of the page.
+ */
+
+Q.Sprite.extend("Cannon", {
+    init: function(p){
+        this._super({
+            asset: 'cannon.png',    // image
+            w: 110,                 // width
+            h: 68,                  // height
+            type: Q.SPRITE_DEFAULT | Q.SPRITE_FRIENDLY, // reacts to sprites mask
+            y: 680,                 // position
+            x: 512,
+            stepDistance: 50       // moving speed
+        }, p);
+        this.add('GunControls, gunControls');
+        Q.input.on('fire', this, "fireGun");
+
+//        this.on("hit.sprite",function(collision) {
+//            if(collision.obj.isA("AlienShot")) {
+//                Q.stageScene("CannonHit", 1);
+//                this.destroy();
+//              }
+//            });
+
+    },
+
+    fireGun: function(){
+        console.log("firing...");
+        Q.audio.play("fire2.mp3");
+    }
+});
+
+
+
+
+    Q.TileLayer.extend("Alien", {
+        init: function(p){
+            this._super({
+
+                tileW: 32,  // Default tile width
+                tileH: 32,  // Default tile height
+                blockTileW: 10,  // Default pre-render size
+                blockTileH: 10,
+                type: Q.SPRITE_DEFAULT // Default type (for collisions)
+
+
+                   // gravity factor
+            }, p);
+            //this.add('2d, stepControls');
+            Q.input.on('fire', this, "fireGun");
+
+            this.on("hit",this,"collision");
+
+        },
+        step: function(dt) {
+            this.p.vy += dt * 2.8;
+
+            this.p.x += this.p.vx * dt;
+            this.p.y += this.p.vy * dt;
+        },
+        fireGun: function(){
+            console.log("firing...");
+            Q.audio.play("fire2.mp3");
+        }
+    });
+
+    Q.UI.Text.extend("Level",{
+        init: function() {
+            this._super({
+                label: "level: 1",
+                align: "right",
+                level: 1,
+                x: Q.width - 70,
+                y: Q.height - 10,
+                weight: "normal",
+                size:18
+            });
+
+            Q.state.on("change.level",this,"level");
+        },
+
+        level: function(lvl) {
+            this.p.label = "level: " + lvl;
+
+        }
+    });
+
+
+
+
 }
--- app/scripts/scenes.js
+++ app/scripts/scenes.js
@@ -6,18 +6,103 @@ Quintus.SpaceInvadersScenes = function(Q) {
     /**
      *  The first level.
      */
-    Q.scene("level1",function(stage) {
+ /*   Q.scene("level1",function(stage) {
         var cannon = stage.insert(new Q.Cannon());
-        var alien=stage.insert(new Q.Alien())
-
-
-
+        var alien = stage.insert(new Q.Alien());
 
         // cleanup
         stage.on("destroy",function() {
             cannon.destroy();
             alien.destroy();
         });
+    });
+
+*/
+
+    Q.scene("hud",function(stage) {
+        stage.insert(new Q.Cannon());
+
+        stage.insert(new Q.Level());
+    }, { stage: 1 });
+
+    function setupLevel(levelAsset,stage) {
+
+       // if(Q.useTiles) {
+            stage.collisionLayer(new Q.GameTiles());
+    //  } else {
+     //       stage.insert(new Q.Background());
+      //  }
+
+       // stage.insert(new Q.BlockTracker({ data: Q.asset(levelAsset) }));
+
+       // stage.insert(new Q.Ball({ x: 50, y: 100 }));
+        //stage.insert(new Q.Countdown());
+    //    stage.insert(new Q.Paddle());
+
+    }
+
+    Q.scene("level1",function(stage) {
+        // Set up the game state
+        Q.state.reset({ score: 0, lives: 3, level: 1 });
+
+        // Add the hud in
+        Q.stageScene("hud");
+
+        // Call the helper methods to get the
+        // level all set up with blocks, a ball and a paddle
+        setupLevel("level1",stage);
+
+        // Set up a listener for when the stage is complete
+        // to load the next level
+        stage.on("complete",function() { Q.stageScene("level2"); });
+    });
+
+    Q.scene("level2",function(stage) {
+        Q.state.set("level",2);
+        setupLevel("level2",stage);
+        stage.on("complete",function() { Q.stageScene("level3"); });
+    });
+
+    Q.scene("level3",function(stage) {
+        Q.state.set("level",3);
+        setupLevel("level3",stage);
+        stage.on("complete",function() { Q.stageScene("level4"); });
+    });
+
+    Q.scene("level4",function(stage) {
+        Q.state.set("level",4);
+        setupLevel("level4",stage);
+        stage.on("complete",function() { Q.stageScene("winner"); });
+    });
+
+    Q.UI.Text.extend("Lives",{
+        init: function() {
+            this._super({
+                label: "lives: 3",
+                align: "left",
+                x: 70,
+                y: Q.height - 10,
+                weight: "normal",
+                size:18
+            });
+
+            Q.state.on("change.lives",this,"lives");
+        },
+
+        lives: function(lives) {
+            this.p.label = "lives: " + lives;
+        }
+    });
+
+    Q.Sprite.extend("Background",{
+        init: function(p) {
+            this._super(p,{
+                x: Q.width/2,
+                y: Q.height/2,
+                asset: 'bg_prerendered.png',
+                type: 0
+            });
+        }
     });
 
 }
