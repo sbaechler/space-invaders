a6b792137e48d9b681533c0f4d2f306c4902c49b
Simon Bächler <simon@stellanera.com>
2013-11-18 12:35:16 +0100

    started tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,9 +1,33 @@
 /*global describe, it */
 'use strict';
 
+
 (function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
+    describe('Sprites Test', function () {
+        var Q, canvas;
+        beforeEach(function() {
+            canvas = document.createElement("canvas");
+            canvas.width = 200;
+            canvas.height = 200;
+
+            Q = Quintus().include("Sprites, SpaceInvadersModels").setup(canvas);
+            Q.clearColor = "#CCC";
+            Q.clear();
+        });
+
+
+        // Don't forget the loop needs to be killed if it exists
+        afterEach(function() {
+            cancelAnimationFrame(Q.loop);
+        });
+        describe('Cannon', function () {
+            var cannon = new Q.Cannon();
+            it('should have the right asset', function(){
+                expect(cannon.asset).toEqual('cannon.png');
+            });
+
+
+
             it('should run here few assertions', function () {
                 expect(true).toBeTruthy();
             });
