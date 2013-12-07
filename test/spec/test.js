88e587a0a7894c7c454b15360252bdbbcbca2b7a
miltom <mili92@gmail.com>
2013-12-07 20:04:55 +0100

    add test

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /**
- * Test aller Game-Objekte
- */
+* Test aller Game-Objekte
+*/
 (function () {
     describe('Sprites Test', function () {
         var Q, canvas, stage;
@@ -19,9 +19,10 @@
             Q.clearColor = "#CCC";
             Q.clear();
             Q.scene("dummy_scene", function(stage) {
-			});
-			Q.stageScene("dummy_scene");
-			stage = Q.stage();
+                        });
+                        Q.stageScene("dummy_scene");
+                        stage = Q.stage();
+            Q.state.reset({ score: 0, lives: 3, level: 1 });
 
             SpriteFixtures.loadSprites(Q);
             waitsFor(SpriteFixtures.doneLoaded,400);
@@ -35,7 +36,7 @@
         });
         describe('Cannon', function () {
             it('should have the right asset', function(){
-                runs(function(){        // wait for Quintus to have finished initialization.
+                runs(function(){ // wait for Quintus to have finished initialization.
                     var cannon = new Q.Cannon();
                     expect(cannon.p.asset).toEqual('cannon.png');
                     expect(cannon.p.collisionMask).toEqual(2);
@@ -52,46 +53,20 @@
             });
             it('should be destroyed if hit', function(){
                 runs(function(){
-                    var cannon = new Q.Cannon();
+                    var cannon = new Q.Cannon({hittable: true});
                     stage.insert(cannon);
                     expect(Q('Cannon').items.length).toBe(1);
+                    expect(Q.state.get('lives')).toBe(3);
                     cannon.trigger('hit');
                     // wait 0.1s
                     waits(100);
                     runs(function(){
                         expect(Q('Cannon').items.length).toBe(0);
+                        expect(Q.state.get('lives')).toBe(2);
                         }
                     );
-
-
                 });
             });
-        });
-        
-        describe('AlienTracker', function () {
-            it('should be able to shoot', function(){
-                runs(function(){
-                    var alien = new Q.AlienTracker();
-                    stage.insert(alien);
-                    expect(Q('AlienShot').length).toBe(1);
-                });
-            });
-
-        });
-        
-        describe('AlienTracker', function () {
-            it('should be able to shoot', function(){
-                runs(function(){
-                    var cannonShot = new Q.CannonShot();
-                    stage.insert(cannonShot);
-                    
-                    var alienShot = new Q.AlienShot();
-                    stage.insert(alienShot);
-                    
-                    cannonShot.collide(alienShot);
-                });
-            });
-
         });
     });
 })();
